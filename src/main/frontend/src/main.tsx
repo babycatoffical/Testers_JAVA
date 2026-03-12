929e82fb2ed7e452ba016fb9040bc1a432c62304
babycatoffical <arazzang880331@gmail.com>
2026-03-12 22:31:44 +0900

    fix. Remove comment and equip store data in the main.tsx

--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { Provider } from 'react-redux'
-// import Store from './store'
+import { Provider } from 'react-redux'
+import store from './store/Store.ts'
 import App from './App.tsx'
 import './index.css'
 
-
 createRoot(document.getElementById('root')!).render(
-    // <Provider store={Store}>
+    <Provider store={store}>
         <StrictMode>
             <App />
         </StrictMode>
-    // {/*</Provider>*/}
+    </Provider>
+
 )
